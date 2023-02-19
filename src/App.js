708ba4dd6c143f0438bf4f310cd65df7b266aeda
Rodrigue Roland CODO <codorol22@gmail.com>
2023-02-19 14:49:26 +0000

    Rendered the board with loop

--- src/App.js
+++ src/App.js
@@ -57,25 +57,25 @@ function Board({squares,xIsNext,onPlay}) {
     }
     onPlay(newSquares);
   }
+  const renderSquare = (i) => {
+    return <Square value={squares[i]} onSquareClick={() => handleClick(i)} />;
+  };
+  const renderBoard = () => {
+    const board = [];
+    for (let i = 0; i < 3; i++) {
+      const row = [];
+      for (let j = 0; j < 3; j++) {
+        row.push(renderSquare(i * 3 + j));
+      }
+      board.push(<div className='board-row'>{row}</div>);
+    }
+    return board;
+  };
   return (
     <div>
         <p className='status'>{showStatus(winner,xIsNext)}</p>
       <div>
-        <div className='board-row'>
-          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        </div>
-        <div className='board-row'>
-          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className='board-row'>
-          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
+        {renderBoard()}
       </div>
     </div>
   );
